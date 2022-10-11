69545c2f96120e9823e1744c55905589eadf2a6b
QingXia-Ela <Elahere@163.com>
2022-10-11 20:21:17 +0800

    feat: 增加动画缓存数组
    有 bug 不知道为什么缓存的动画对象使用的数组是旧的随机点位数组，但是控制台打印的是新数组

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Envelope from "./components/envelope";
 import Particles from "./pages/particles";
+import NewStruct from "./pages/NewStruct";
+
 const App = () => {
   return (
     <div className="layout">
       {/* <Envelope /> */}
-      <Particles />
+      {/* <Particles /> */}
+      <NewStruct />
     </div>
   );
 };
